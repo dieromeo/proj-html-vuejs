2be9ea978ba2eb63db8475ebca1c7a92d4a6961a
dieromeo <diegoromeo99@icloud.com>
2023-12-19 13:52:48 +0100

    feat: font awesome import

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faArrowLeftLong, faArrowRightLong, faMagnifyingGlass, faBars)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
