ca04e8f29bbc808153a75e481ce2013eb678d3f3
dieromeo <diegoromeo99@icloud.com>
2023-12-19 14:21:59 +0100

    feat: topbar

--- src/components/Header/Navbar.vue
+++ src/components/Header/Navbar.vue
@@ -1,40 +1,64 @@
 <script>
+import SmallRightArrow from '../SmallRightArrow.vue';
+
 export default {
     name: 'Navbar',
     data() {
         return {
             list: [
-                { title: 'Homes', link: '#' },
-                { title: 'Pages', link: '#' },
-                { title: 'Blog', link: '#' },
-                { title: 'Shop', link: '#' },
-                { title: 'Events', link: '#' },
-                { title: 'Elements', link: '#' },
+                { title: 'Homes', link: '#', active: true },
+                { title: 'Pages', link: '#', active: false },
+                { title: 'Blog', link: '#', active: false },
+                { title: 'Shop', link: '#', active: false },
+                { title: 'Events', link: '#', active: false },
+                { title: 'Elements', link: '#', active: false },
             ]
-        }
-    }
+        };
+    },
+    components: { SmallRightArrow }
 }
 </script>
 
 <template>
     <nav>
-        freccia
+        <SmallRightArrow class="orange" />
         <ul>
             <li v-for="element in list">
-                <a :href="element.link">{{ element.title.toUpperCase() }}</a>
+                <a :href="element.link" :class="element.active === true ? 'orange' : ''">{{ element.title.toUpperCase()
+                }}</a>
             </li>
         </ul>
-        icone
+        <font-awesome-icon icon="magnifying-glass" />
+        <font-awesome-icon icon="bars" />
     </nav>
 </template>
 
 <style lang="scss" scoped>
+@use '../../style/partials/variables.scss' as *;
+
+.orange {
+    color: $secondary-color;
+}
+
 nav {
     display: flex;
+    align-items: center;
 
     ul {
         display: flex;
         gap: 45px;
+        margin-left: 10px;
+        font-size: 13px;
+    }
+
+    a {
+        color: $text-main;
+        font-weight: bold;
+    }
+
+    svg {
+        margin-left: 40px;
+        cursor: pointer;
     }
 }
 </style>
---
+++ src/components/SmallRightArrow.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'SmallRightArrow'
+}
+</script>
+
+<template>
+    <div class="right-arrow">
+        &rightarrow;
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.right-arrow {
+    font-size: 25px;
+}
+</style>
