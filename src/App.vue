4f8f7585cd2da06ef6c28e087de9c208a87bcc4e
dieromeo <diegoromeo99@icloud.com>
2023-12-21 17:38:04 +0100

    feat: scroll page arrow

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script>
+import BottomArrow from './components/BottomArrow.vue';
 import FooterComponent from './components/Footer/FooterComponent.vue';
 import HeaderComponent from './components/Header/HeaderComponent.vue';
 import MainComponent from './components/Main/MainComponent.vue';
 
 export default {
-    components: { HeaderComponent, MainComponent, FooterComponent }
+    components: { HeaderComponent, MainComponent, FooterComponent, BottomArrow }
 }
 </script>
 
 <template>
+    <BottomArrow />
     <HeaderComponent />
     <MainComponent />
     <FooterComponent />
---
+++ src/components/BottomArrow.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+    name: 'BottomArrow',
+    data() {
+        return {
+            active: false
+        };
+    },
+    // aggiunta del metodo allo scroll di window
+    mounted() {
+        window.addEventListener('scroll', this.setScroll);
+    },
+
+    methods: {
+        // imposta i pixel scrollati
+        setScroll() {
+            this.active = window.scrollY;
+        },
+        // fa ritornare la pagina in cima
+        scrollToTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="scroll-to-top" v-if="active" @click="scrollToTop">
+        <span class="arrow-up">&ShortUpArrow;</span>
+    </div>
+</template>
+  
+
+  
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+.scroll-to-top {
+    position: fixed;
+    bottom: 20px;
+    right: 0;
+    z-index: 999;
+    cursor: pointer;
+    background-color: $white;
+    color: $secondary-color;
+    border: 1px solid $text-secondary;
+    border-radius: 50%;
+    width: 45px;
+    height: 45px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.arrow-up {
+    font-size: 24px;
+}
+</style>
